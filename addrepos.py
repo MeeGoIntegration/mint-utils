c8773655494088d7ab4b41aebe6902e957badd21
Islam Amer <islam.amer@nokia.com>
2011-12-05 00:04:35 +0200

    fix

--- addrepos.py
+++ addrepos.py
@@ -38,7 +38,10 @@ def do_addrepos(self, subcmd, opts, myproject, targetproject):
             if not archs:
                 print "no archs enabled in %s, skipping" % reponame
                 continue
-            repo_element = ET.Element('repository', name="%s_%s" % (targetproject.replace(":","_"), "_".join(archs)))
+            new_reponame = "%s_%s_%s" % (targetproject.replace(":","_"), 
+                                         reponame,
+                                         "_".join(archs))
+            repo_element = ET.Element('repository', name=new_reponame)
             repo_element.append(ET.Element('path', repository=reponame, project=targetproject))
             for arch in archs:
                 arch_element = ET.Element('arch')
--- linkprj.py
+++ linkprj.py
@@ -70,9 +70,11 @@ def do_linkprj(self, subcmd, opts, new_project, link_to_project):
             if not archs:
                 print "no archs enabled in %s, skipping" % reponame
                 continue
-            repo_element = ET.Element('repository', name="%s_%s" % (link_to_project.replace(":","_"), "_".join(archs)),
-                                      linkedbuild="localdep"
-                                     )
+            new_reponame = "%s_%s_%s" % (link_to_project.replace(":","_"), 
+                                         reponame,
+                                         "_".join(archs))
+            repo_element = ET.Element('repository', name=new_reponame,
+                                      linkedbuild="localdep")
             repo_element.append(ET.Element('path', repository=reponame, project=link_to_project))
             for arch in archs:
                 arch_element = ET.Element('arch')
